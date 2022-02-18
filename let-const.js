ef6e6a903cf04df6bdbbffe6a89af78a20dff20f
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-19 00:20:21 +0600

    let and const explored

---
+++ let-const.js
@@ -0,0 +1,29 @@
+
+
+// const balance = 1240;
+// balance = 1340;
+
+let balance = 1230
+balance = 1000;
+
+const userName = 'wife';
+const weTogether = 'Husband' + ' ' + userName;
+console.log(weTogether);
+// userName = "janu"
+
+const numbers = [23, 423, 53, 42, 2345];
+// numbers = [34, 523, 524, 523]  // not allowed
+numbers.push(555);
+numbers[1] = 333;
+
+const student = {roll: 101, name: 'kamrul', job: 'intern'};
+student.name = 'Hasan';
+
+// student = {name: "hasan"} // not allowed
+
+let sum = 0;
+for(let i = 0; i < numbers.length; i++){
+    const number = number[i];
+    console.log(number);
+    sum = sum + number;  
+}
